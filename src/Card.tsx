811860e8fe0cb9a1664c05cdbc8a7fbf0c62fc7a
Alvaro <alvaro.hulsehabet@gmail.com>
2021-06-14 17:36:31 -0600

    Card styling method change

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,8 @@
 import "./scss/Card.scss";
 import icon from "./svg/icon.svg";
-import rhombus from "./svg/rhombus-red.svg";
 import InputSet from "./Input";
 
-import { Formik, Form, Field } from "formik";
+import { Formik, Form } from "formik";
 
 function Card() {
     return (
@@ -25,25 +24,25 @@ function Card() {
                 setSubmitting(false);
             }}>
                 <Form>
-                    <InputSet id="personal" icon={rhombus} inputs={[
+                    <InputSet id="personal" icon="" inputs={[
                         { "type": "text", "name": "name" },
-                        { "type": "text", "name": "occupation" }
+                        { "type": "text", "name": "jobtitle" }
                     ]}/>
 
-                    <fieldset className="card-section" id="cellular">
-                        <Field type="text" name="phonenumber"/>
-                        <Field type="text" name="altphonenumber"/>
-                    </fieldset>
+                    <InputSet id="cellular" icon="" inputs={[
+                        { "type": "text", "name": "phonenumber"},
+                        { "type": "text", "name": "altphonenumber"}
+                    ]}/>
 
-                    <fieldset className="card-section" id="emails">
-                        <Field type="email" name="personalemail"/>
-                        <Field type="email" name="businessemail"/>
-                    </fieldset>
+                    <InputSet id="emails" icon="" inputs={[
+                        { "type": "email", "name": "personalemail"},
+                        { "type": "email", "name": "businessemail"}
+                    ]}/>
 
-                    <fieldset className="card-section" id="address">
-                        <Field type="text" name="address"/>
-                        <Field type="text" name="mailing"/>
-                    </fieldset>
+                    <InputSet id="address" icon="" inputs={[
+                        { "type": "text", "name": "address"},
+                        { "type": "text", "name": "mailing"}
+                    ]}/>
                 </Form>
             </Formik>
         </div>
