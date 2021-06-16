c99efc18129c2e88060b2c90f23ce0e62f1c9468
Alvaro <alvaro.hulsehabet@gmail.com>
2021-06-16 15:56:53 -0600

    Discarding the old design

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Card from './Card';
 function App() {
     return (
         <div className="App">
-            <Card v="v1" name="Fire Nation" motto="We do Invasions" logo={icon}/>
+            {/* <Card v="v1" name="Fire Nation" motto="We do Invasions" logo={icon}/> */}
             <Card v="v2" name="Fire Nation" motto="We do Invasions" logo={icon}/>
         </div>
     );
